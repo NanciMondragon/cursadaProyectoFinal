acf7a2bcfdac266e8305484aa1c3e7ad620ab450
NanciMondragon <nantzamon@gmail.com>
2022-11-08 23:08:46 -0600

    VisualÃiza en pantalla las recetas por categoria

--- js/app.js
+++ js/app.js
@@ -3,6 +3,10 @@
 function iniciarApp(){
 
     const selectCategorias = document.querySelector('#categorias');
+    selectCategorias.addEventListener('change', seleccionarCategoria)
+
+    const resultado = document.querySelector('#resultado');
+    
 
     obtenerCategorias();
 
@@ -21,12 +25,62 @@ function iniciarApp(){
             option.value = strCategory
             option.textContent = strCategory
             selectCategorias.appendChild(option);
-            
-
         });
-
     }
 
+    function seleccionarCategoria(e) {
+        const categoria = e.target.value;
+        const url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${categoria}`;
+        fetch(url) 
+            .then(respuesta => respuesta.json())
+            .then(resultado => mostrarRecetas(resultado.meals))
+
+    } 
+
+    function mostrarRecetas(recetas = []){
+       
+        recetas.forEach(receta => {
+
+            const { idMeal, strMeal, strMealThumb } = receta
+        
+            const recetaContenedor = document.createElement('div');
+            recetaContenedor.classList.add('col-md-4');
+            
+            const recetaCard = document.createElement('div');
+            recetaCard.classList.add('card', 'mb-4');
+
+            const recetaImagen = document.createElement('img');
+            recetaImagen.classList.add('card-img-top');
+            recetaImagen.alt = `Receta ${strMeal}`;
+            recetaImagen.src = strMealThumb;
+
+            const recetaCardBody = document.createElement('div');
+            recetaCardBody.classList.add('card-body');
+
+            const recetaHeading = document.createElement('h3');
+            recetaHeading.classList.add('card-title', 'mb-3');
+            recetaHeading.textContent = strMeal;
+
+            const recetaButton = document.createElement('button');
+            recetaButton.classList.add('btn', 'btn-danger', 'w-100');
+            recetaButton.textContent = "Visualizar Receta";
+
+            recetaCardBody.appendChild(recetaHeading);
+            recetaCardBody.appendChild(recetaButton);
+
+            recetaCard.appendChild(recetaImagen);
+            recetaCard.appendChild(recetaCardBody);
+
+            recetaContenedor.appendChild(recetaCard);
+
+            resultado.appendChild(recetaContenedor);
+            
+            
+           
+
+        })
+
+    }
 }
 
 document.addEventListener('DOMContentLoaded', iniciarApp);
